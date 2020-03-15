a9d7adadc285f1ad2f6166aeb74a6fbbc7125efc
Nader <nader_abdelghani@hotmail.com>
2020-03-16 00:14:22 +0200

    Fix Lock Declaration Error

--- Pc1.py
+++ Pc1.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     manager = multiprocessing.Manager()
     masterDataFile = manager.dict # masterDataFile = { ip1: { port1: [ file1, file2, ... ], port2: [...], ... }, ip2: {...} }
     dataKeepersState = manager.dict # dataKeepersState = { ip1: { port1: True, port2: False, ... }, ip2: { port1: True, ... }, ... }
-    syncLock = manager.RLock
+    syncLock = multiprocessing.RLock()
     filesDictionary = manager.dict # filesDictionary = { filename1: [ { ip1: [port1, port2, ...], ip2: [...], ... } , instanceCount], filename2: [...] }
     # filesDictionary["filenameKey.mp4"][1] = instanceCount
     # filesDictionary["filenameKey.mp4"][0]["tcp:127.0.0.1"] = [8000, 8001, 8002]
