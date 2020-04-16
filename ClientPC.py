6a94e69060c867f70d033960ed923c03be986e0f
Nader <nader_abdelghani@hotmail.com>
2020-04-16 22:55:47 +0200

    Refactoring

--- ClientPC.py
+++ ClientPC.py
@@ -1,29 +1,48 @@
-import multiprocessing 
+import multiprocessing
 import Client
-    
+
 
 if __name__ == '__main__':
-    numberOfprocessesOfMaster=5 #number of processes  multi-process(MasterTracker)
-    startingPortMasterClient=7000 #first port between client/master
-    startingPortDatakeeperClient=8000 #first port between client/datakeeper
-    masterIp="tcp://172.30.249.130:" #master ip
+    
+    ##############################################################################################################################################
+    ################# PLEASE EITHER HARDCODE THE FOLLOWING IPS AND COMMENT THE FOLLOWING INPUT BLOCK OR INPUT THEM THROUGH CLI ###################
+    ##############################################################################################################################################
+    
+    masterIP = "172.30.249.130"  # master ip
+    
+    ################################################################################
+    ################################## INPUT BLOCK #################################
+    ################################################################################
+    
+    print("/////////////////////////////////////////////////////////////////////\n")
+    print("//////////////// Please enter the master server IP //////////////////\n")
+    print("/////////////////////////////////////////////////////////////////////\n")
+    input(masterIP)
+    
+    ##############################################################################################################################################
+    
+    # number of processes  multi-process(MasterTracker)
+    numberOfprocessesOfMaster = 5
+    startingPortMasterClient = 7000  # first port between client/master
+    masterIP = "tcp://" + masterIP + ":"
     processes=[]
     
     print("Enter # of client processes:")
-    numberOfClients=input()
+    numberOfClients = input()
     for i in range(int(numberOfClients)):
-        commands=[]
-        print("Enter # of commands for process #%d"%i)
-        n=input()
+        commands = []
+        print("Enter # of commands for process #%d" % i)
+        n = input()
         for j in range(int(n)):
-            print("command #%d:"%j)
-            x=input()
+            print("command #%d:" % j)
+            x = input()
             commands.append(x)
-        t=multiprocessing.Process(target=Client.client,args=(masterIp,startingPortMasterClient,numberOfprocessesOfMaster,commands))
+        t = multiprocessing.Process(target=Client.client, args=(
+            masterIP, startingPortMasterClient, numberOfprocessesOfMaster, commands))
         processes.append(t)
 
     for j in processes:
         j.start()
-        
+
     for j in processes:
         j.join()
--- DataKeeperPC.py
+++ DataKeeperPC.py
@@ -1,37 +1,54 @@
-import multiprocessing 
-#import Master    
+import multiprocessing
+#import Master
 #import client
 import DataKeeper
-    
+
 
 if __name__ == '__main__':
-    numberOfprocessesOfMaster=5 #number of processes  multi-process(MasterTracker)
-    numberOfprocessesOfNodes=3 #number of processes  multi-process(data keeper)
-    numberOfNodes=1 #number of nodes of data keeper
-    startingPortMasterClient=7000 #first port between client/master
-    startingPortDatakeeperClient=8000 #first port between client/datakeeper
-    masterIp="tcp://127.0.0.1:" #master ip
-    replicatesCount=3 # count of replicates
-    processes=[]
-    manager = multiprocessing.Manager()
-    masterDataFile = manager.dict() # masterDataFile = { ip1: { port1: [ file1, file2, ... ], port2: [...], ... }, ip2: {...} }
-    dataKeepersState = manager.dict() # dataKeepersState = { ip1: { port1: True, port2: False, ... }, ip2: { port1: True, ... }, ... }
-    syncLock = multiprocessing.RLock()
-    iAmAliveDict = manager.dict()
-    filesDictionary = manager.dict() # filesDictionary = { filename1: [ { ip1: [port1, port2, ...], ip2: [...], ... } , instanceCount], filename2: [...] }
-    # filesDictionary["filenameKey.mp4"][1] = instanceCount
-    # filesDictionary["filenameKey.mp4"][0]["tcp:127.0.0.1"] = [8000, 8001, 8002]
+    
+    ##############################################################################################################################################
+    ################# PLEASE EITHER HARDCODE THE FOLLOWING IPS AND COMMENT THE FOLLOWING INPUT BLOCK OR INPUT THEM THROUGH CLI ###################
+    ##############################################################################################################################################
+    
+    masterIP = "172.30.249.130"  # master ip
+    datakeeperIP = "172.30.38.151"  # datakeeper ip
+    
+    ################################################################################
+    ################################## INPUT BLOCK #################################
+    ################################################################################
+    
+    print("/////////////////////////////////////////////////////////////////////\n")
+    print("//////////////// Please enter the master server IP //////////////////\n")
+    print("/////////////////////////////////////////////////////////////////////\n")
+    input(masterIP)
 
-   
+    print("/////////////////////////////////////////////////////////////////////\n")
+    print("////////////// Please enter the datakeeper server IP ////////////////\n")
+    print("/////////////////////////////////////////////////////////////////////\n")
+    input(datakeeperIP)
+    
+    ##############################################################################################################################################
+    
+    # number of processes  multi-process(MasterTracker)
+    numberOfprocessesOfMaster = 5
+    # number of processes  multi-process(data keeper)
+    numberOfprocessesOfNodes = 3
+    numberOfNodes = 1  # number of nodes of data keeper
+    startingPortMasterClient = 7000  # first port between client/master
+    startingPortDatakeeperClient = 8000  # first port between client/datakeeper
+    
+    replicatesCount = 3  # count of replicates
+    processes = []
+
     for i in range(numberOfNodes):
         for k in range(numberOfprocessesOfNodes):
-            t= multiprocessing.Process(target=DataKeeper.dataKeeper,args=(i,k,startingPortDatakeeperClient,numberOfprocessesOfMaster,masterIp)) 
+            t = multiprocessing.Process(target=DataKeeper.dataKeeper, args=(
+                i, k, startingPortDatakeeperClient, numberOfprocessesOfMaster, masterIP, datakeeperIP))
             processes.append(t)
-
 
     for j in processes:
         j.start()
-        
+
     for j in processes:
         j.join()
     print("Done!")
